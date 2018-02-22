258b9c0fb9350edf34dee1e8e336c745f47aae5c
metacontext <Torsten.Schrade@adwmainz.de>
2018-02-22 16:36:21 +0100

    Cleanup

--- ext_emconf.php
+++ ext_emconf.php
@@ -16,7 +16,6 @@ $EM_CONF[$_EXTKEY] = array(
     'constraints' => array(
         'depends' => array(
             'typo3' => '7.6.0-8.7.99',
-//          'chf_time' => '',
             'vocabulary' => '',
         ),
         'conflicts' => array(),
