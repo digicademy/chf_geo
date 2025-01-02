0feb2de7129237b1045a02b5d36607b866a0e8cf
metacontext <Torsten.Schrade@adwmainz.de>
2025-01-02 18:59:32 +0100

    Add v12 compatible includes of static TypoScript and TSConfig

---
+++ Configuration/TCA/Overrides/sys_template.php
@@ -0,0 +1,8 @@
+<?php
+defined('TYPO3') or die();
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
+    'chf_geo',
+    'Configuration/TypoScript/',
+    'Cultural Heritage Framework: Geo Component'
+);
--- ext_tables.php
+++ ext_tables.php
@@ -1,12 +1,2 @@
 <?php
 defined('TYPO3') or die();
-
-// TYPOSCRIPT
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('chf_geo', 'Configuration/TypoScript/', 'Cultural Heritage Framework: Geo Component');
-
-// TSCONFIG
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('
-    <INCLUDE_TYPOSCRIPT: source="FILE:EXT:chf_geo/Configuration/TSConfig/setup.txt">
-');
